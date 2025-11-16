686e40893e6e4aad90aa47e76c9dcbe201ccbe3a
Ashwanth A <ashwanthreddy002@gmail.com>
2025-11-16 06:17:35 +0000

    yeah buddy dont add unnecessary things , remove the map section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,18 +28,6 @@ export default function ContactPage() {
                 </Card>
             </div>
         </section>
-
-        <section className="w-full h-[400px] md:h-[500px]">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.608359483329!2d-74.0088204845941!3d40.71426997933182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda22d%3A0x8a5a3c35b8c8a1e!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1620841123456!5m2!1sen!2sin"
-                width="100%"
-                height="100%"
-                style={{ border: 0 }}
-                allowFullScreen={false}
-                loading="lazy"
-                title="Google Map of New York"
-            ></iframe>
-        </section>
       </div>
     </>
   );
